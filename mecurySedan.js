6af8e6a25512a726c3d114ba03568d52355bbc67
Fallanagel <havemedance@hotmail.com>
2022-06-07 22:53:48 -0500

    almost got the Car class

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,3 +4,8 @@ const VehicleModule = require("./vehicleBaseClass")
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class Car extends Vehicle.Vehicle {
+     constructor(maximumPassengers, passenger, numberOfWheels, maximumSpeed, fuel, scheduleService){
+
+     }}
